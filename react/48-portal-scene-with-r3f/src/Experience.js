95f89ee44c2ea7c20491042869d3da69a2a1bf11
Mariana  Cruz <mda-cruz@Marianas-Air-2.Home>
2023-01-20 17:41:26 +0000

    workaround bug and portal scene done

--- react/48-portal-scene-with-r3f/src/Experience.js
+++ react/48-portal-scene-with-r3f/src/Experience.js
@@ -1,11 +1,36 @@
-import { Center, useTexture, useGLTF, OrbitControls } from "@react-three/drei";
+import {
+  shaderMaterial,
+  Center,
+  useTexture,
+  useGLTF,
+  OrbitControls,
+  Sparkles,
+} from "@react-three/drei";
+import portalVertexShader from "./shaders/portal/vertex.js";
+import portalFragmentShader from "./shaders/portal/fragment.js";
+import * as THREE from "three";
+import { extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#ffffff"),
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
+extend({ PortalMaterial });
 
 export default function Experience() {
   const { nodes } = useGLTF("./model/portalSceneup.glb");
   const bakedTexture = useTexture("./model/bake.jpg");
   bakedTexture.flipY = false;
-
-  console.log(nodes);
+  const portalMaterial = useRef();
+  useFrame((state, delta) => {
+    portalMaterial.current.uTime += delta;
+  });
 
   return (
     <>
@@ -17,11 +42,40 @@ export default function Experience() {
         </mesh>
         <mesh
           geometry={nodes.poleLigh1.geometry}
-          position={nodes.poleLigh1.position}
-        />
+          position={[
+            nodes.poleLigh1.position.x + 0.02,
+            nodes.poleLigh1.position.y - 0.84,
+            nodes.poleLigh1.position.z - 1.563,
+          ]}
+        >
+          <meshBasicMaterial color={"#ffffe5"} />
+        </mesh>
         <mesh
           geometry={nodes.poleLight2.geometry}
-          position={nodes.poleLight2.position}
+          position={[
+            nodes.poleLight2.position.x + 0.02,
+            nodes.poleLight2.position.y - 0.84,
+            nodes.poleLight2.position.z - 1.563,
+          ]}
+        >
+          <meshBasicMaterial color={"#ffffe5"} />
+        </mesh>
+        <mesh
+          geometry={nodes.Circle.geometry}
+          position={[
+            nodes.Circle.position.x + 0.02,
+            nodes.Circle.position.y - 0.84,
+            nodes.Circle.position.z - 1.563,
+          ]}
+        >
+          <portalMaterial ref={portalMaterial} />
+        </mesh>
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-z={-1.5}
+          speed={0.2}
+          count={40}
         />
       </Center>
     </>
